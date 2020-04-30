e456e24c04f4c5073b81a8fbbf42b03b35904d16
hansa-lallu <hansalallu@gmail.com>
2020-04-30 07:53:18 +0100

    continues to askf or user input until user enters exit

--- lib/echo.rb
+++ lib/echo.rb
@@ -4,7 +4,18 @@ class Echo
   end
 
   def input
-    input = STDIN.gets.chomp
+    @input = STDIN.gets.chomp
+  end
+
+  def run
+    loop do
+      prompt
+      input
+      if @input == 'exit'
+        print 'Goodbye'
+        break
+      end
+    end
   end
 end 
 
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -14,6 +14,14 @@ describe Echo do
       expect(echo.input).to eq('Hello World')
     end
   end
+
+  describe '#run' do 
+    it 'continues to prompt user for input ' do
+      allow(STDIN).to receive(:gets).and_return('Hello, World')
+      allow(echo).to receive(:loop).and_yield
+      expect { echo.run }.to output('Say something: ').to_stdout
+    end 
+  end
 end 
 
 
