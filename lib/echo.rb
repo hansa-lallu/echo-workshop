0d54773d865d71004075163cef7bb1f6411e39ec
hansa-lallu <hansalallu@gmail.com>
2020-04-30 07:48:51 +0100

    getting user input

--- lib/echo.rb
+++ lib/echo.rb
@@ -2,6 +2,10 @@ class Echo
   def prompt
     print "Say something: "
   end
+
+  def input
+    input = STDIN.gets.chomp
+  end
 end 
 
 
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -7,6 +7,13 @@ describe Echo do
       expect { echo.prompt }.to output('Say something: ').to_stdout
     end
   end
+
+  describe '#input' do
+    it 'outputs the input from user' do
+      allow(STDIN).to receive(:gets).and_return('Hello World')
+      expect(echo.input).to eq('Hello World')
+    end
+  end
 end 
 
 
